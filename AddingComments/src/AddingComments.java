a82c5280b52fa900fa4a9e568d2071c1753dbad3
StillWaters <ndivhalenioscar624@gmail.com>
2024-12-01 12:46:47 +0200

    Add files via upload

---
+++ AddingComments/src/AddingComments.java
@@ -0,0 +1,19 @@
+/**
+ *
+ * @author Oscar
+ */
+public class AddingComments {
+
+    public static void main(String [] args){
+        
+        
+        System.out.println("* * * *" +  " * *      *");      //  printing a star
+        System.out.println("*     *" +  " *  *     *");      //  My initials
+        System.out.println("*     *" +  " *   *    *");      //  I am star
+        System.out.println("*     *" +  " *    *   *");      // Always Shining
+        System.out.println("*     *" +  " *     *  *");      // Within and Outside
+        System.out.println("* * * *" +  " *      * *");      // I made this
+       
+    
+    }
+}
